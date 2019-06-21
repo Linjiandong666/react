1cf63e70b820c61550463bee7c6f544336070163
lin jian dong <2544608795@qq.com>
2019-06-21 13:51:12 +0800

    '新建了需要的文件'

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Route} from "react-router-dom";
+
+import App from './App';
+
+
+ReactDOM.render(<Route><App/></Route>,document.getElementById('root'));
