9e69af01added346a125ae43f1deb0c297ed4d97
lin jian dong <2544608795@qq.com>
2019-06-24 20:34:13 +0800

    完成了title随点击菜单显示功能，且已进行优化

--- src/components/header-main/index.jsx
+++ src/components/header-main/index.jsx
@@ -7,7 +7,7 @@ import {withRouter} from 'react-router-dom';
 //处理时间的库
 import dayjs from 'dayjs';
 import {reqWeather} from "../../api";
-
+import menuList from '../../config/menu-config';
 
 class HeaderMain extends Component {
 
@@ -33,11 +33,21 @@ class HeaderMain extends Component {
     }
   }
 
-  //得到用户名展示(一次即可)
+
   componentWillMount() {
+    //得到用户名展示(一次即可)
     this.username=getItem().username;
+    //初始化渲染在render前得到title
+    this.title=this.getTitle(this.props);
+    // console.log(this.title)
   };
 
+
+  componentWillReceiveProps(nextProps) {
+    //在setState前得到title，消除更新状态反复调用getTitle函数
+    this.title=this.getTitle(nextProps);
+    // console.log(this.title)
+  }
 
   //退出登录
   logout=()=>{
@@ -55,6 +65,33 @@ class HeaderMain extends Component {
     });
   };
 
+  //获取title
+  getTitle=(nextProps)=>{
+    //获取地址路径
+    const {pathname}=nextProps.location;
+    //对一级菜单，二级菜单分别进行遍历，看key值是否能跟路径匹配，匹配就把菜单title赋值给初始化的title
+    for (let i = 0; i < menuList.length; i++) {
+      const menu=menuList[i];
+
+      if(menu.children){
+
+        for (let j = 0; j < menu.children.length; j++) {
+          const item= menu.children[j];
+          if(item.key===pathname){
+            return item.title;
+          }
+        }
+
+      } else {
+
+        if(menu.key===pathname){
+          return menu.title;
+        }
+
+      }
+    }
+  };
+
 
 
   render() {
@@ -66,7 +103,7 @@ class HeaderMain extends Component {
         <MyButton onClick={this.logout}>退出</MyButton>
       </div>
       <div className="header-main-bottom">
-        <span className="header-main-left">用户管理</span>
+        <span className="header-main-left">{this.title}</span>
         <div className="header-main-right">
           <span>{dayjs(systime).format('YYYY-MM-DD HH:mm:ss')}</span>
           <img src={weatherImg}/>
